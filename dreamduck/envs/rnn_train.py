badd608b84dae6df9d7db50df734554210f3df56
Frank Roeder <frankyroeder@icloud.com>
2019-07-10 01:12:56 +0200

    in progress

--- dreamduck/envs/rnn_train.py
+++ dreamduck/envs/rnn_train.py
@@ -1,22 +1,16 @@
 import numpy as np
 import os
 import json
-import random
 import time
-import tensorflow as tf
 
-from rnn.rnn import reset_graph, HyperParams, MDNRNN, get_pi_idx
+from vae.vae import reset_graph
+from rnn.rnn import HyperParams, MDNRNN
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 np.set_printoptions(precision=4, edgeitems=6, linewidth=100, suppress=True)
 
-model_save_path = "tf_rnn"
-model_rnn_size = 512
-model_num_mixture = 5
-model_restart_factor = 10.
-
 DATA_DIR = "series"
-
+model_save_path = "tf_rnn"
 if not os.path.exists(model_save_path):
     os.makedirs(model_save_path)
 
@@ -25,19 +19,30 @@ if not os.path.exists(initial_z_save_path):
     os.makedirs(initial_z_save_path)
 
 
+def random_batch():
+    indices = np.random.permutation(N_data)[0:batch_size]
+    mu = data_mu[indices]
+    logvar = data_logvar[indices]
+    action = data_action[indices]
+    s = logvar.shape
+    z = mu + np.exp(logvar/2.0) * np.random.randn(*s)
+    return z, action
+
+
 def default_hps():
     return HyperParams(
-        # train on sequences of 500 (found it worked better than 1000)
-        max_seq_len=500,
-        seq_width=64,    # width of our data (64)
-        rnn_size=model_rnn_size,    # number of rnn cells
+        num_steps=4000,
+        # train on sequences of 1000 (so 999 + teacher forcing shift)
+        max_seq_len=999,
+        # width of our data (32 + 3 actions)
+        input_seq_width=35,
+        output_seq_width=64,    # width of our data
+        rnn_size=256,    # number of rnn cells
         batch_size=100,   # minibatch sizes
         grad_clip=1.0,
-        num_mixture=model_num_mixture,   # number of mixtures in MDN
-        # factor of importance for restart=1 rare case for loss.
-        restart_factor=model_restart_factor,
+        num_mixture=5,   # number of mixtures in MDN
         learning_rate=0.001,
-        decay_rate=0.99999,
+        decay_rate=1.0,
         min_learning_rate=0.00001,
         # set this to 1 to get more stable results (less chance of NaN), but slower
         use_layer_norm=0,
@@ -47,170 +52,67 @@ def default_hps():
         input_dropout_prob=0.90,
         use_output_dropout=0,
         output_dropout_prob=0.90,
-        is_training=1)
+        is_training=1
+    )
 
 
 hps_model = default_hps()
 hps_sample = hps_model._replace(
-    batch_size=1, max_seq_len=2, use_recurrent_dropout=0, is_training=0)
+    batch_size=1, max_seq_len=1, use_recurrent_dropout=0, is_training=0)
+
+raw_data = np.load(os.path.join(DATA_DIR, "series.npz"))
 
 # load preprocessed data
-raw_data = np.load(os.path.join(DATA_DIR, "series.npz"), allow_pickle=True)
-raw_data_mu = raw_data["mu"]
-raw_data_logvar = raw_data["logvar"]
-raw_data_action = raw_data["action"]
-raw_data_restart = raw_data["restart"]
-# raw_data_reward = raw_data["reward"]
-# raw_data_reward = (raw_data_reward - raw_data_reward.min()) / \
-#    (raw_data_reward.max() - raw_data_reward.min()) * 2. + (-1.)
-
-
-def load_series_data():
-    all_data = []
-    for i in range(len(raw_data_mu)):
-        action = raw_data_action[i]
-        mu = raw_data_mu[i]
-        logvar = raw_data_logvar[i]
-        restart = raw_data_restart[i]
-        # reward = raw_data_reward[i]
-        all_data.append([mu, logvar, action, restart])
-    return all_data
-
-
-def get_frame_count(all_data):
-    frame_count = []
-    for data in all_data:
-        frame_count.append(len(data[0]))
-    return np.sum(frame_count)
-
-
-def create_batches(all_data, batch_size=100, seq_length=500):
-    num_frames = get_frame_count(all_data)
-
-    num_batches = int(num_frames/(batch_size*seq_length))
-    num_frames_adjusted = num_batches*batch_size*seq_length
-    random.shuffle(all_data)
-    num_frames = get_frame_count(all_data)
-    data_mu = np.zeros((num_frames, N_z), dtype=np.float16)
-    data_logvar = np.zeros((num_frames, N_z), dtype=np.float16)
-    data_action = np.zeros((num_frames, 2), dtype=np.float16)
-    data_restart = np.zeros(num_frames, dtype=np.uint8)
-    # data_reward = np.zeros(num_frames, dtype=np.float16)
-    idx = 0
-    for data in all_data:
-        mu, logvar, action, restart = data
-        N = len(action)
-        data_mu[idx:idx+N] = mu.reshape(N, 64)
-        data_logvar[idx:idx+N] = logvar.reshape(N, 64)
-        data_action[idx:idx+N] = action.reshape(N, 2)
-        data_restart[idx:idx+N] = restart.reshape(N)
-        # data_reward[idx:idx+N] = reward.reshape(N)
-        # data_restart[idx] = 1
-        idx += N
-
-    data_mu = data_mu[0:num_frames_adjusted]
-    data_logvar = data_logvar[0:num_frames_adjusted]
-    data_action = data_action[0:num_frames_adjusted]
-    data_restart = data_restart[0:num_frames_adjusted]
-    # data_reward = data_reward[0:num_frames_adjusted]
-
-    data_mu = np.split(data_mu.reshape(batch_size, -1, 64), num_batches, 1)
-    data_logvar = np.split(data_logvar.reshape(
-        batch_size, -1, 64), num_batches, 1)
-    data_action = np.split(data_action.reshape(
-        batch_size, -1, 2), num_batches, 1)
-    data_restart = np.split(data_restart.reshape(
-        batch_size, -1), num_batches, 1)
-    # data_reward = np.split(data_reward.reshape(
-    #    batch_size, -1), num_batches, 1)
-
-    return data_mu, data_logvar, data_action, data_restart
-
-
-def get_batch(batch_idx, data_mu, data_logvar, data_action, data_restart):
-    batch_mu = data_mu[batch_idx]
-    batch_logvar = data_logvar[batch_idx]
-    batch_action = data_action[batch_idx]
-    batch_restart = data_restart[batch_idx]
-    # batch_reward = data_reward[batch_idx]
-    batch_s = batch_logvar.shape
-    batch_z = batch_mu + np.exp(batch_logvar/2.0) * np.random.randn(*batch_s)
-    return batch_z, batch_action, batch_restart  # , batch_reward
-
-
-# process data
-all_data = load_series_data()
-
-
+data_mu = raw_data["mu"]
+data_logvar = raw_data["logvar"]
+data_action = raw_data["action"]
 max_seq_len = hps_model.max_seq_len
-N_z = hps_model.seq_width
+
+N_data = len(data_mu)
+batch_size = hps_model.batch_size
 
 # save 1000 initial mu and logvars:
-initial_mu = []
-initial_logvar = []
-for i in range(1000):
-    mu = np.copy(raw_data_mu[i][0, :]*10000).astype(np.int).tolist()
-    logvar = np.copy(raw_data_logvar[i][0, :]*10000).astype(np.int).tolist()
-    initial_mu.append(mu)
-    initial_logvar.append(logvar)
+initial_mu = np.copy(data_mu[:1000, 0, :]*10000).astype(np.int).tolist()
+initial_logvar = np.copy(
+    data_logvar[:1000, 0, :]*10000).astype(np.int).tolist()
 with open(os.path.join("tf_initial_z", "initial_z.json"), 'wt') as outfile:
     json.dump([initial_mu, initial_logvar], outfile,
               sort_keys=True, indent=0, separators=(',', ': '))
 
 reset_graph()
-model = MDNRNN(hps_model, gpu_mode=tf.test.is_gpu_available())
+rnn = MDNRNN(hps_model)
 
 hps = hps_model
 start = time.time()
+for local_step in range(hps.num_steps):
 
-for epoch in range(1, 4000):
-    # print('preparing data for epoch', epoch)
-    data_mu, data_logvar, data_action, data_restart = \
-        0, 0, 0, 0
-    data_mu, data_logvar, data_action, data_restart = \
-        create_batches(all_data)
-    num_batches = len(data_mu)
-    # print('number of batches', num_batches)
-    end = time.time()
+    step = rnn.sess.run(rnn.global_step)
+    curr_learning_rate = (hps.learning_rate-hps.min_learning_rate) * \
+        (hps.decay_rate) ** step + hps.min_learning_rate
+
+    raw_z, raw_a = random_batch()
+    inputs = np.concatenate((raw_z[:, :-1, :], raw_a[:, :-1, :]), axis=2)
+    outputs = raw_z[:, 1:, :]  # teacher forcing (shift by one predictions)
+
+    feed = {
+        rnn.input_x: inputs,
+        rnn.output_x: outputs,
+        rnn.lr: curr_learning_rate
+    }
+    (train_cost, state, train_step, _) = rnn.sess.run([
+        rnn.cost, rnn.final_state, rnn.global_step, rnn.train_op
+    ], feed)
+    if (step % 20 == 0 and step > 0):
+        end = time.time()
     time_taken = end-start
-    # print('time taken to create batches', time_taken)
-
-    batch_state = model.sess.run(model.initial_state)
-
-    for local_step in range(num_batches):
-
-        batch_z, batch_action, batch_restart = get_batch(
-            local_step, data_mu, data_logvar, data_action, data_restart)
-        step = model.sess.run(model.global_step)
-        curr_learning_rate = (hps.learning_rate-hps.min_learning_rate) * \
-            (hps.decay_rate) ** step + hps.min_learning_rate
-
-        feed = {model.batch_z: batch_z,
-                model.batch_action: batch_action,
-                model.batch_restart: batch_restart,
-                # model.batch_reward: batch_reward,
-                model.initial_state: batch_state,
-                model.lr: curr_learning_rate}
-
-        (train_cost, z_cost, r_cost, batch_state, train_step, _) = \
-            model.sess.run([
-                model.cost,
-                model.z_cost,
-                model.r_cost,
-                model.final_state,
-                model.global_step,
-                model.train_op
-            ], feed)
-        if (step % 20 == 0 and step > 0):
-            end = time.time()
-            time_taken = end-start
-            start = time.time()
-            output_log = "step: %d, lr: %.6f, cost: %.4f, z_cost: %.4f, r_cost: %.4f, train_time_taken: %.4f" % (
-                step, curr_learning_rate, train_cost, z_cost, r_cost, time_taken)
-            print(output_log)
-    if epoch % 25 == 0:
-        model.save_json(os.path.join(model_save_path, "rnn{}.json".format(epoch)))
+    start = time.time()
+    output_log = "step: %d, lr: %.6f, cost: %.4f, train_time_taken: %.4f" % \
+        (step, curr_learning_rate, train_cost, time_taken)
+    print(output_log)
+    if local_step % 500 == 0:
+        rnn.save_json(os.path.join(
+            model_save_path, "rnn_{}.json".format(local_step)))
 
 
 # save the model (don't bother with tf checkpoints json all the way ...)
-model.save_json(os.path.join(model_save_path, "rnn.json"))
+rnn.save_json(os.path.join(model_save_path, "rnn.json"))
--- dreamduck/envs/rnnenv.py
+++ dreamduck/envs/rnnenv.py
@@ -4,19 +4,29 @@ import sys
 import argparse
 from gym import spaces
 from gym.spaces.box import Box
-from dreamduck.envs.rnn.rnn import reset_graph, rnn_model_path_name,\
-    model_rnn_size, model_state_space, MDNRNN, hps_sample, get_pi_idx
-from dreamduck.envs.vae.vae import ConvVAE, vae_model_path_name
-from dreamduck.envs.env import DuckieTownWrapper
+from dreamduck.envs.rnn.rnn import get_pi_idx, hps_sample, MDNRNN, rnn_init_state, rnn_next_state, rnn_output, rnn_output_size
+from dreamduck.envs.vae.vae import ConvVAE
 import os
 from gym.utils import seeding
-from cv2 import resize
 import tensorflow as tf
 import gym
 
 SCREEN_X = 64
 SCREEN_Y = 64
 TEMPERATURE = 1.25
+# controls whether we concatenate (z, c, h), etc for features used for car.
+MODE_ZCH = 0
+MODE_ZC = 1
+MODE_Z = 2
+MODE_Z_HIDDEN = 3
+MODE_ZH = 4
+
+EXP_MODE = MODE_ZH
+
+with open(os.path.join('initial_z', 'initial_z.json'), 'r') as f:
+    [initial_mu, initial_logvar] = json.load(f)
+
+initial_mu_logvar = [list(elem) for elem in zip(initial_mu, initial_logvar)]
 
 model_path_name = 'dreamduck/envs/tf_initial_z'
 
@@ -29,73 +39,60 @@ class DuckieTownRNN(gym.Env):
         'video.frames_per_second': 50
     }
 
-    def __init__(self, render_mode=False, load_model=True):
-        super(DuckieTownRNN, self).__init__()
-        self.render_mode = render_mode
+    def __init__(self, agent, load_model=True):
+        self.env_name = "carracing"
+        self.vae = ConvVAE(batch_size=1, gpu_mode=False, is_training=False, reuse=True)
 
-        with open(os.path.join(model_path_name, 'initial_z.json'), 'r') as f:
-            [initial_mu, initial_logvar] = json.load(f)
-        self.initial_mu_logvar = [list(elem)
-                                  for elem in zip(initial_mu, initial_logvar)]
-
-        reset_graph()
-
-        self.vae = ConvVAE(batch_size=1, gpu_mode=tf.test.is_gpu_available(),
-                           is_training=False, reuse=True)
-        self.rnn = MDNRNN(hps_sample, gpu_mode=tf.test.is_gpu_available())
+        self.rnn = MDNRNN(hps_sample, gpu_mode=False, reuse=True)
 
         if load_model:
-            self.vae.load_json(os.path.join(vae_model_path_name, 'vae.json'))
-            self.rnn.load_json(os.path.join(rnn_model_path_name, 'rnn.json'))
+            self.vae.load_json('vae/vae.json')
+            self.rnn.load_json('rnn/rnn.json')
 
-        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,))
+        self.state = rnn_init_state(self.rnn)
+        self.rnn_mode = True
 
-        self.outwidth = self.rnn.hps.seq_width
-        self.obs_size = self.outwidth + model_rnn_size*model_state_space
+        self.input_size = rnn_output_size(EXP_MODE)
+        self.z_size = 32
 
-        self.observation_space = Box(
-            low=-50., high=50., shape=(self.obs_size,))
+        if EXP_MODE == MODE_Z_HIDDEN: # one hidden layer
+            self.hidden_size = 40
+            self.weight_hidden = np.random.randn(self.input_size, self.hidden_size)
+            self.bias_hidden = np.random.randn(self.hidden_size)
+            self.weight_output = np.random.randn(self.hidden_size, 3)
+            self.bias_output = np.random.randn(3)
+            self.param_count = ((self.input_size+1)*self.hidden_size) + (self.hidden_size*3+3)
+        else:
+            self.weight = np.random.randn(self.input_size, 3)
+            self.bias = np.random.randn(3)
+            self.param_count = (self.input_size)*3+3
 
-        self.zero_state = self.rnn.sess.run(self.rnn.zero_state)
+        self.render_mode = False
+        self.observation_space = Box(low=-50., high=50., shape=(64))
         self._seed()
-
-        self.rnn_state = None
-        self.z = None
-        self.restart = None
-        self.temperature = None
-        self.frame_count = None
+        self.agent = agent
+        self.vae = agent.vae
+        self.rnn = agent.rnn
+        self.z_size = self.rnn.hps.output_seq_width
         self.viewer = None
-
-        self.reward = 0
-        # TODO: Change
-        self.max_frame = 2100
-        self.np_random = np.random
-
+        self.frame_count = None
+        self.z = None
+        self.temperature = 0.7
+        self.vae_frame = None
         self._reset()
 
-    def _sample_init_z(self):
-        idx = self.np_random.randint(0, len(self.initial_mu_logvar))
-        init_mu, init_logvar = self.initial_mu_logvar[idx]
-        init_mu = np.array(init_mu)/10000.
-        init_logvar = np.array(init_logvar)/10000.
-        init_z = init_mu + np.exp(init_logvar/2.0) * \
-            self.np_random.randn(*init_logvar.shape)
-        return init_z
-
-    def _current_state(self):
-        if model_state_space == 2:
-            return np.concatenate([
-                self.z, self.rnn_state.c.flatten(), self.rnn_state.h.flatten()
-            ], axis=0)
-        return np.concatenate([self.z, self.rnn_state.h.flatten()], axis=0)
+    def _sample_z(self, mu, logvar):
+        z = mu + np.exp(logvar/2.0) * self.np_random.randn(*logvar.shape)
+        return z
 
     def _reset(self):
-        self.temperature = TEMPERATURE
-        self.rnn_state = self.zero_state
-        self.z = self._sample_init_z()
-        self.restart = 1
+        idx = self.np_random.randint(0, len(initial_mu_logvar))
+        init_mu, init_logvar = initial_mu_logvar[idx]
+        init_mu = np.array(init_mu)/10000.
+        init_logvar = np.array(init_logvar)/10000.
+        self.z = self._sample_z(init_mu, init_logvar)
         self.frame_count = 0
-        return self._current_state()
+        return self.z
 
     def _seed(self, seed=None):
         if seed:
@@ -103,42 +100,25 @@ class DuckieTownRNN(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def _step(self, action):
-
-        self.frame_count += 1
-
-        prev_z = np.zeros((1, 1, self.outwidth))
-        prev_z[0][0] = self.z
-
-        prev_action = np.reshape(action, (1, 1, 2))
-
-        prev_restart = np.ones((1, 1))
-        prev_restart[0] = self.restart
-
-        # prev_reward = np.ones((1, 1))
-        # TODO: Is this right? If yes remove comment
-        # prev_reward[0][0] = self.reward
-
+    def _sample_next_z(self, action):
         s_model = self.rnn
         temperature = self.temperature
 
-        feed = {s_model.input_z: prev_z,
-                s_model.input_action: prev_action,
-                s_model.input_restart: prev_restart,
-                # s_model.input_reward: prev_reward,
-                s_model.initial_state: self.rnn_state
-                }
+        sess = s_model.sess
+        hps = s_model.hps
 
-        [logmix, mean, logstd, logrestart, next_state] = \
-            s_model.sess.run([s_model.out_logmix,
-                              s_model.out_mean,
-                              s_model.out_logstd,
-                              s_model.out_restart_logits,
-                              # s_model.reward_logits,
-                              s_model.final_state],
-                             feed)
+        OUTWIDTH = hps.output_seq_width
 
-        OUTWIDTH = self.outwidth
+        prev_x = np.zeros((1, 1, OUTWIDTH))
+        prev_x[0][0] = self.z
+
+        strokes = np.zeros((1, OUTWIDTH), dtype=np.float32)
+
+        input_x = np.concatenate((prev_x, action.reshape(1, 1, 3)), axis=2)
+        feed = {s_model.input_x: input_x,
+                s_model.initial_state: self.agent.state}
+        [logmix, mean, logstd, self.agent.state] = sess.run(
+            [s_model.out_logmix, s_model.out_mean, s_model.out_logstd, s_model.final_state], feed)
 
         # adjust temperatures
         logmix2 = np.copy(logmix)/temperature
@@ -156,31 +136,27 @@ class DuckieTownRNN(gym.Env):
             chosen_logstd[j] = logstd[j][idx]
 
         rand_gaussian = self.np_random.randn(OUTWIDTH)*np.sqrt(temperature)
-        next_z = chosen_mean+np.exp(chosen_logstd) * rand_gaussian
+        next_x = chosen_mean+np.exp(chosen_logstd)*rand_gaussian
 
-        next_restart = 0
+        next_z = next_x.reshape(OUTWIDTH)
+
+        return next_z
+
+    def _step(self, action):
+        self.frame_count += 1
+        next_z = self._sample_next_z(action)
+        reward = 0
         done = False
-        if (logrestart[0] > 0):
-            next_restart = 1
+        if self.frame_count > 1200:
             done = True
-
         self.z = next_z
-        self.restart = next_restart
-        self.rnn_state = next_state
-        # _, reward, _, _ = super(DuckieTownRNN, self)._step(action)
-        reward = 1
+        return next_z, reward, done, {}
 
-        if self.frame_count >= self.max_frame:
-            done = True
-
-        return self._current_state(), reward, done, {}
-
-    def _get_image(self, upsize=False):
-        img = self.vae.decode(self.z.reshape(1, 64)) * 255.
+    def decode_obs(self, z):
+        # decode the latent vector
+        img = self.vae.decode(z.reshape(1, self.z_size)) * 255.
         img = np.round(img).astype(np.uint8)
         img = img.reshape(64, 64, 3)
-        if upsize:
-            img = resize(img, (800, 600))
         return img
 
     def _render(self, mode='human', close=False):
